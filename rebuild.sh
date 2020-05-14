c142a62d525bec526d88c42a722614f5d07a247b
eleph-hub <eleph@private.net>
2020-05-14 21:43:00 +0100

    limit log file size

--- rebuild.sh
+++ rebuild.sh
@@ -3,5 +3,5 @@
 docker build -t "youtube-dl-server" .
 docker stop youtube-dl
 docker rm youtube-dl
-docker run -d --restart=always -p 6060:8080 --name youtube-dl -v /media/Malaga:/youtube-dl:z youtube-dl-server
+docker run -d --restart=always -p 6060:8080 --name youtube-dl --log-opt max-size=10m --log-opt max-file=5 -v /media/Malaga:/youtube-dl:z youtube-dl-server
 docker system prune
