e3ad4baa675e56851beefafa1f16b6f894a10a25
krizleebear <krizleebear@users.noreply.github.com>
2020-12-26 23:33:33 +0100

    docker pull on build

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker build -t "youtube-dl-server" .
+docker build --pull -t "youtube-dl-server" .
 docker stop youtube-dl
 docker rm youtube-dl
 docker run -d --restart=always -p 6060:8080 --name youtube-dl --log-opt max-size=10m --log-opt max-file=5 -v /media/Malaga:/youtube-dl:z youtube-dl-server
